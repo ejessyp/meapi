d361bb641d49c2f44e8bf5445cc322c8a9f09e43
ejessyp <ejessyp@gmail.com>
2021-09-15 18:27:41 +0100

    database.js updated

--- db/database.js
+++ db/database.js
@@ -1,14 +1,18 @@
 // MongoDB
 const mongo = require("mongodb").MongoClient;
-const config = require("./config.json");
+
 const collectionName = "files";
 
 const database = {
     getDb: async function getDb () {
-        let dsn = `mongodb+srv://${config.username}:${config.password}@cluster0.pwqp2.mongodb.net/editor?retryWrites=true&w=majority`;
+
+        let dsn;
 
         if (process.env.NODE_ENV === 'test') {
             dsn = "mongodb://localhost:27017/test";
+        }  else {
+            const config = require("./config.json");
+            dsn = `mongodb+srv://${config.username}:${config.password}@cluster0.pwqp2.mongodb.net/editor?retryWrites=true&w=majority`;
         }
 
         const client  = await mongo.connect(dsn, {
